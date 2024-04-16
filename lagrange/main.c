27e8d69e311ab508e1c65957b5d6f87d40cd28e1
nathaliaatamanchuk <nathaliaatamanchuk5@gmail.com>
2024-04-16 16:34:33 -0300

    feat: header

--- lagrange/main.c
+++ lagrange/main.c
@@ -1,3 +1,14 @@
+/* ESTE CÓDIGO FOI REALIZADO PELO GRUPO:
+   Anna Júlia Pereira de Oliveira Andrade, RA: 234659
+   Kaique Vecchia Alves, RA: 235446
+   Nathalia Atamanchuk Baleeiro, RA: 235215
+
+    <- DESCRIÇÃO ->
+    O código foi desenvolvido para calcular a interpolação de Newton através da Forma de Lagrange,
+    onde o usuário insere o grau do polinômio e os pontos, tão como um valor de X, e então o programa
+    calcula e exibe os valores Lk(x) enquanto o usuário desejar.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
